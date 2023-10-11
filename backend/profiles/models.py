633c435306faab4c382be2f835c0f85199ed6491
Megan <megantech@hotmail.com>
2023-10-11 12:20:37 +0000

    Update image field to show default image from cloudinary

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -9,9 +10,12 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/',
-        default='../blank-profile-picture-973460_1280_fxozyw'
+    image = CloudinaryField(
+        'image',
+        default=(
+            'https://res.cloudinary.com/dnkoqrvie/image/upload/'
+            'v1693398098/blank-profile-picture-973460_1280_fxozyw.webp'
+        )
     )
 
     class Meta:
