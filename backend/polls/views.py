834ca4b39ed84fc3c257b86a96b9679209b83582
Megan <megantech@hotmail.com>
2023-10-24 16:35:04 +0000

    Create views in views.py

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Poll, Vote
+from .serializers import PollSerializer, VoteSerializer
 
-# Create your views here.
+
+class PollList(generics.ListCreateAPIView):
+    serializer_class = PollSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Poll.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PollSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Poll.objects.all()
+
+
+class VoteCreate(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
