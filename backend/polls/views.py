6de3e86a41d4aba72874e5b2f9c915bf8d8ebad0
Megan <megantech@hotmail.com>
2023-10-24 16:40:14 +0000

    Add backend.permissions to polls views

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from backend.permissions import IsOwnerOrReadOnly
 from .models import Poll, Vote
 from .serializers import PollSerializer, VoteSerializer
 
