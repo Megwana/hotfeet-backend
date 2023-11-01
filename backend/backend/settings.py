313e90dbd2b083e62aaba292975fcdb23d1a54e7
Megan <megantech@hotmail.com>
2023-11-01 17:14:55 +0000

    Corrected mistype on pagination. Root_route now showing in server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,11 +79,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
-    'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DATETIME_FORMAT': '%d %b %Y'
-    }
+    'DATETIME_FORMAT': '%d %b %Y',
+}
 
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
